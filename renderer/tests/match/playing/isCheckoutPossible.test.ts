9450ce184ff7b970c4d1521c5fd658587cae92fb
Tim Deres <50623315+timderes@users.noreply.github.com>
2026-01-12 15:23:10 +0100

    feat: add tests for `isCheckoutPossible`
    Introduces unit tests for the isCheckoutPossible function to verify correct checkout route detection, handling of used throws, and cases where no checkout is possible.

---
+++ renderer/tests/match/playing/isCheckoutPossible.test.ts
@@ -0,0 +1,55 @@
+import { describe, expect, it } from "vitest";
+import isCheckoutPossible from "@/lib/playing/isCheckoutPossible";
+import { GameState } from "@/types/GameState";
+import { THROWS_PER_ROUND } from "@/utils/constants";
+
+describe("match.playing.isCheckoutPossible", () => {
+  const mockState = {
+    currentPlayerIndex: 0,
+  };
+
+  it("returns the correct checkout route when possible", () => {
+    const updatedState = {
+      ...mockState,
+      matchRound: [
+        { score: 60, isDouble: false },
+        { score: 0, isDouble: false },
+      ],
+      players: [{ scoreLeft: 80 }],
+      // Omit other properties with the as GameState type assertion
+    } as GameState;
+
+    const result = isCheckoutPossible(updatedState);
+
+    expect(result).toEqual(["D10"]); // Example checkout for 20 remaining
+  });
+  it("returns undefined the player has used all throws", () => {
+    const updatedState = {
+      ...mockState,
+      matchRound: Array.from({ length: THROWS_PER_ROUND }, () => ({
+        score: 20,
+        isDouble: false,
+      })),
+
+      players: [{ scoreLeft: 141 }],
+      // Omit other properties with the as GameState type assertion
+    } as GameState;
+
+    const result = isCheckoutPossible(updatedState);
+
+    expect(result).toBeUndefined();
+  });
+
+  it("returns undefined when no checkout is possible (bogey number 179)", () => {
+    const updatedState = {
+      ...mockState,
+      matchRound: [{ score: 0, isDouble: false }],
+      players: [{ scoreLeft: 179 }], // No checkout for 179 in default table
+      // Omit other properties with the as GameState type assertion
+    } as GameState;
+
+    const result = isCheckoutPossible(updatedState);
+
+    expect(result).toBeUndefined();
+  });
+});
