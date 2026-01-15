ff83251c092e21ab258d506f6300bab6e00cf672
Tim Deres <50623315+timderes@users.noreply.github.com>
2026-01-15 17:22:04 +0100

    feat: enhance profile page with stats and localization
    Updated the profile/all page to display detailed user statistics using the Stat component and improved layout with Mantine UI elements. Added a localized string for 'playing with app since' in both English and German. Refactored Stat component to accept NumberFormatterProps directly.

--- renderer/pages/[locale]/profile/all/index.tsx
+++ renderer/pages/[locale]/profile/all/index.tsx
@@ -4,13 +4,30 @@ import { getStaticPaths, makeStaticProperties } from "@lib/getStatic";
 import useGetAllProfiles from "@/hooks/getAllProfiles";
 
 import DefaultLayout, { headerHeight } from "@components/layouts/Default";
-import { Grid, NavLink, ScrollArea } from "@mantine/core";
+import {
+  Card,
+  Divider,
+  Flex,
+  Grid,
+  Group,
+  NavLink,
+  ScrollArea,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
 import ProfileAvatar from "@/components/content/ProfileAvatar";
 import { useEffect, useState } from "react";
 import type { Profile } from "@/types/profile";
+import getFormattedName from "@/utils/misc/getFormattedName";
+import Stat from "@/components/content/Stat";
+import { APP_NAME, DATE_OPTIONS } from "@/utils/constants";
 
 const ProfileAllPage = () => {
-  const { t } = useTranslation();
+  const {
+    t,
+    i18n: { language: locale },
+  } = useTranslation();
   const profiles = useGetAllProfiles();
   const [activeProfile, setActiveProfile] = useState<Profile | undefined>(
     undefined,
@@ -25,7 +42,6 @@ const ProfileAllPage = () => {
       <Grid gutter={0}>
         <Grid.Col span={3}>
           <ScrollArea.Autosize
-            bg="dark.8"
             mih={`calc(100dvh - ${headerHeight}px)`}
             mah={`calc(100dvh - ${headerHeight}px)`}
           >
@@ -37,6 +53,7 @@ const ProfileAllPage = () => {
                 leftSection={<ProfileAvatar profile={profile} size="sm" />}
                 label={profile.username}
                 onClick={() => setActiveProfile(profile)}
+                variant="filled"
               />
             ))}
           </ScrollArea.Autosize>
@@ -44,9 +61,50 @@ const ProfileAllPage = () => {
         <Grid.Col span={9}>
           <ScrollArea.Autosize mah={`calc(100dvh - ${headerHeight}px)`}>
             {profiles && activeProfile ? (
-              <div>
-                <h2>{activeProfile.username}</h2>
-              </div>
+              <Card component={Stack}>
+                <Flex align="start" justify="end">
+                  CONTROLS
+                </Flex>
+                <Group>
+                  <ProfileAvatar profile={activeProfile} size="xl" />
+                  <div>
+                    <Title>{getFormattedName(activeProfile.name)} </Title>
+                    <Text opacity={0.7}>@{activeProfile.username}</Text>
+                  </div>
+                </Group>
+                <Text>
+                  {t("profile:playingWithAppSince", {
+                    APP_NAME,
+                    DATE: new Date(activeProfile.createdAt).toLocaleDateString(
+                      locale,
+                      DATE_OPTIONS,
+                    ),
+                  })}
+                </Text>
+                <Divider />
+                <Group grow>
+                  <Stat
+                    text={t("stats.matches")}
+                    value={activeProfile.statistics.playedMatches}
+                    decimalScale={2}
+                  />
+                  <Stat
+                    text={t("stats.avg")}
+                    value={activeProfile.statistics.average}
+                    decimalScale={2}
+                  />
+                  <Stat
+                    text={t("stats.dartsThrown")}
+                    value={activeProfile.statistics.thrownDarts}
+                    decimalScale={2}
+                  />
+                  <Stat
+                    text={t("stats.180s")}
+                    value={activeProfile.statistics.thrownOneHundredAndEighty}
+                    decimalScale={2}
+                  />
+                </Group>
+              </Card>
             ) : undefined}
           </ScrollArea.Autosize>
         </Grid.Col>
@@ -57,6 +115,6 @@ const ProfileAllPage = () => {
 
 export default ProfileAllPage;
 
-export const getStaticProps = makeStaticProperties(["common"]);
+export const getStaticProps = makeStaticProperties(["common", "profile"]);
 
 export { getStaticPaths };
